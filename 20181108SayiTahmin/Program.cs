81d8d23b2e03fe7598ce4501897e97213b46caeb
MELİSA ECE SURAL <47081681+MelisaEceSural@users.noreply.github.com>
2019-08-27 19:05:08 +0300

    Add files via upload

---
+++ 20181108SayiTahmin/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20181108SayiTahmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Klavyeden girilen sayıyı bilgisayardan tahmin ettirmeeye çalışan uygulama. Amaç girilen sayının uygulama tarafından kaçıncı denemesinde bulduğunu söylemek.
+            Random rnd = new Random();// sayı tahmini için (sayı üretmek için) gerekli olan random sınıfının nesnesi
+            Console.Write("Tahmin edilecek sayıyı girin: ");
+            int sayi = int.Parse(Console.ReadLine());
+            bool dongu = true;
+            int sayac = 0, uretilenSayi;
+            //do
+            //{
+            //    uretilenSayi = rnd.Next(0, 101);
+            //    sayac++;
+            //    if (uretilenSayi == sayi)
+            //    {
+            //        dongu = false;
+            //    }
+
+            //} while (dongu);// aşağıda daha pratiği var
+            do
+            {
+                uretilenSayi = rnd.Next(0, 101);
+                sayac++;
+            } while (uretilenSayi!=sayi);
+
+
+            Console.WriteLine($"{sayi} sayısı, {sayac}. denemede tahmin edildi.");
+            Console.ReadKey();
+
+        }
+    }
+}
